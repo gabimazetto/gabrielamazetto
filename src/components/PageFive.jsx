ad82f6a27a2e00d295c6d526c94d5628d7a4d6ce
gabimazetto <gmazettog@gmail.com>
2023-07-10 15:55:50 -0300

    Links

--- src/components/PageFive.jsx
+++ src/components/PageFive.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 
 export function PageFive() {
-    return(
+    return (
         <div className="container-projects">
             <div className="content-projects">
                 <div className="header-projects">
@@ -11,43 +11,59 @@ export function PageFive() {
                 <div className="projects">
                     <div className="projects-wrapper">
                         <div className="project-info">
-                            <h3>FoodExpress</h3>
-                            <p>Projeto final do bootcamp Soulcode, feito em equipe. Simula um app de delivery de comidas, com contexto de cliente e de restaurantes.</p>
+                            <h3>FoodExpress Front</h3>
+                            <p>Front-End do projeto final do bootcamp Soulcode, feito em equipe. Simula um app de delivery de comidas, com contexto de cliente e de restaurantes.</p>
                         </div>
                         <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/foodexpress" target="_blank" rel="noopener noreferrer">
                             <i className="devicon-github-original icon-medium"></i>
-                            <span>Código</span>
+                            </a>
                         </div>
                     </div>
                     <div className="projects-wrapper">
                         <div className="project-info">
-                            <h3>SoulPet</h3>
-                        <p>Projeto em grupo do bootcamp Soulcode. Simula um sistema de PetShop, onde é possível agendar procedimentos, consultar produtos, etc.</p>
+                            <h3>FoodExpress Back</h3>
+                            <p>Back-End do projeto final do bootcamp Soulcode, feito em equipe. Simula um app de delivery de comidas, com contexto de cliente e de restaurantes.</p>
                         </div>
                         <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/foodexpress" target="_blank" rel="noopener noreferrer">
                             <i className="devicon-github-original icon-medium"></i>
-                            <span>Código</span>
+                            </a>
                         </div>
                     </div>
+                    <div className="projects-wrapper">
+                        <div className="project-info">
+                            <h3>SoulPet Front</h3>
+                            <p>Front-End do projeto em grupo do bootcamp Soulcode. Simula um sistema de PetShop, onde é possível agendar procedimentos, consultar produtos, etc.</p>
+                        </div>
+                        <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/soulpet-front" target="_blank" rel="noopener noreferrer">
+                            <i className="devicon-github-original icon-medium"></i>
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="projects-wrapper">
+                        <div className="project-info">
+                            <h3>SoulPet Back</h3>
+                            <p>Back-End do projeto em grupo do bootcamp Soulcode. Simula um sistema de PetShop, onde é possível agendar procedimentos, consultar produtos, etc.</p>
+                        </div>
+                        <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/soulpet-back" target="_blank" rel="noopener noreferrer">
+                            <i className="devicon-github-original icon-medium"></i>
+                            </a>
+                        </div>
+                    </div>
+
                     <div className="projects-wrapper">
                         <div className="project-info">
                             <h3>Bibliotech</h3>
                             <p>Projeto em grupo do bootcamp Soulcode. Simula uma biblioteca virtual, com sistema de empréstimos e vendas.</p>
                         </div>
                         <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/bibliotech-gabi" target="_blank" rel="noopener noreferrer">
                             <i className="devicon-github-original icon-medium"></i>
-                            <span>Código</span>
-                        </div>
-                    </div>
-                    <div className="projects-wrapper">
-                        <div className="project-info">
-                            <h3>Goodpay</h3>
-                            <p>Projeto em grupo do bootcamp Soulcode. Simula um site de finanças.</p>
-                        </div>
-                        <div className="project-icon">
-                            <i className="devicon-github-original icon-medium">
-                            </i>
-                            <span>Código</span>
+                            </a>
                         </div>
                     </div>
                     <div className="projects-wrapper">
@@ -56,14 +72,15 @@ export function PageFive() {
                             <p>Com objetivo de fornecer uma visão abrangente das minhas habilidades e realizações, bem como estabelecer conexões profissionais. </p>
                         </div>
                         <div className="project-icon">
+                            <a href="https://github.com/gabimazetto/gabrielamazetto" target="_blank" rel="noopener noreferrer">
                             <i className="devicon-github-original icon-medium">
                             </i>
-                            <span>Código</span>
+                            </a>
                         </div>
                     </div>
-                    
+
                 </div>
             </div>
         </div>
-)
+    )
 }
