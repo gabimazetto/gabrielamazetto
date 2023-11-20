5502aa7f3fbc9617a2e0a6bdaed05bd5b9f95b31
gabimazetto <gmazettog@gmail.com>
2023-11-20 11:19:34 -0300

    Atualização

--- src/components/PageOne.jsx
+++ src/components/PageOne.jsx
@@ -13,7 +13,7 @@ export function PageOne() {
                 
                 <div className="text-profile">
                     <p>Me chamo Gabriela, mas todos me chamam de "gabi". Nasci, cresci e moro em Curitiba, PR. Tenho 27 anos e estou em transição de carreira, saindo da área da educação (cursava Letras pela UFPR) para área da tecnologia. Além de vídeo aulas e workshops, recentemente finalizei um bootcamp em React.js e Node.js pela Soulcode Academy. </p> <br />
-                    <p>Quando comecei a estudar programação, achei que focaria no front-end, por ser mais criativa e gostar da parte visual (já trabalhei como social media), mas o back-end foi aos poucos me conquistando e aqui estou eu, uma dev fullstack. </p>
+                    <p>Por ser mais criativa e bastante visual, o Front-End me conquistou desde o começo. Gosto muito de ver os projetos que estou fazendo tomando forma. Ainda estou só no começo, mas planejo ver várias páginas programadas por mim, ou com minha ajuda, por aí. </p>
                 </div>
                     
             </div>
